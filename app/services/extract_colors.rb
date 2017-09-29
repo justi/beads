df1d9f0c64d8314ca30478a2aec9cbe09abeaaba
Justyna <justine84@gmail.com>
2017-09-29 13:18:44 +0000

    Update extracting colors

--- app/services/extract_colors.rb
+++ app/services/extract_colors.rb
@@ -1,30 +1,27 @@
 class ExtractColors
     def initialize(dominant_colors: Miro::DominantColors)
         @dominant_colors = dominant_colors
-        
     end
     
     def call(bead_pack)
         Miro.options[:method] = 'pixel_group'
-        bead_pack_id = bead_pack.id
         colors = dominant_colors.new bead_pack.picture.path if bead_pack.picture.present?
         rgb = colors.to_rgb
         percentage = colors.by_percentage
         Miro.options[:method] = 'histogram'
         colors = dominant_colors.new bead_pack.picture.path if bead_pack.picture.present?
-        hls = colors.to_hsl
-        
-        zip_three_arrays(rgb, hls, percentage).each do |result|
-            color = ColorFactory.create_color_from_array(result)
-            if color
-                stworz posrednia
-            end
+        hsl = colors.to_hsl
+        rgb.zip(hsl).zip(percentage).each do |result|
+            result_colors = result.first
+            result_percentage = result.second
+            bead_color = ColorFactory.create_color_from_array(result_colors)
+            bcp = BeadColorPack.create(
+                bead_color: bead_color,
+                bead_pack: bead_pack,
+                color_percentage: result_percentage
+            ) if bead_color
         end
-        
-    end
-    
-    def zip_three_arrays(rgb, hls, percentage)
-        rgb.zip(hls, percentage) { |x, y, z| p [x, y, z] }
+        binding.pry
     end
     
     private
