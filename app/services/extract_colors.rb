83dba8a86ad9b30440a48a9ae9a2d471347724a5
Justyna <justine84@gmail.com>
2017-09-29 11:46:55 +0000

    Extracting data from image

--- app/services/extract_colors.rb
+++ app/services/extract_colors.rb
@@ -1,13 +1,24 @@
 class ExtractColors
     def initialize(dominant_colors: Miro::DominantColors)
         @dominant_colors = dominant_colors
+        
     end
     
     def call(bead_pack)
-        binding.pry
+        Miro.options[:method] = 'pixel_group'
         bead_pack_id = bead_pack.id
         colors = dominant_colors.new bead_pack.picture.path if bead_pack.picture.present?
-        colors.to_hex
+        rgb = colors.to_rgb
+        percentage = colors.by_percentage
+        Miro.options[:method] = 'histogram'
+        colors = dominant_colors.new bead_pack.picture.path if bead_pack.picture.present?
+        hls = colors.to_hsl
+        binding.pry
+        result = zip_three_arrays(rgb, hls, percentage)
+    end
+    
+    def zip_three_arrays(rgb, hls, percentage)
+        rgb.zip(hls, percentage) { |x, y, z| p [x, y, z] }
     end
     
     private
