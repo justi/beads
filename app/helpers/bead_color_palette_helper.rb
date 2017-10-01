ecdaea6040c4730b9814d63d270002b1c62dde50
Justyna <justine84@gmail.com>
2017-10-01 20:00:12 +0000

    Display percentage color value

--- app/helpers/bead_color_palette_helper.rb
+++ app/helpers/bead_color_palette_helper.rb
@@ -6,7 +6,7 @@ module BeadColorPaletteHelper
     bead_color_packs.each do |bcp|
       color = bcp.bead_color
       content_style = "display: block; float: left;  width: 35px; height: 35px; background-color:rgb(#{color.to_rgb})"
-      color_string += link_to content_tag(:span, ' ', style: content_style), bead_color_path(color)
+      color_string += link_to content_tag(:span, ' ', style: content_style, title: "#{bcp.to_s}"), bead_color_path(color)
     end
     content_tag :div, color_string.html_safe, style: "display: inline-block;"
   end
--- app/models/bead_color_pack.rb
+++ app/models/bead_color_pack.rb
@@ -1,4 +1,8 @@
 class BeadColorPack < ActiveRecord::Base
   belongs_to :bead_color
   belongs_to :bead_pack
+  
+  def to_s
+    "#{(self.color_percentage*100).round}%"
+  end
 end
