92420783a1e70f27d3c149b09424a4e3fadefa2f
Justyna <justine84@gmail.com>
2017-10-03 13:49:19 +0000

    Update layouts

--- app/helpers/bead_color_palette_helper.rb
+++ app/helpers/bead_color_palette_helper.rb
@@ -5,8 +5,8 @@ module BeadColorPaletteHelper
     color_string = ''
     bead_color_packs.each do |bcp|
       color = bcp.bead_color
-      content_style = "display: block; float: left;  width: 35px; height: 35px; background-color:rgb(#{color.to_rgb})"
-      color_string += link_to content_tag(:span, ' ', style: content_style, title: "#{bcp.to_s}"), bead_color_path(color)
+      content_style = "display: block; float: left;  width: 30px; height: 30px; background-color:rgb(#{color.to_rgb}); margin: 1px;"
+      color_string += link_to content_tag(:span, ' ', class: 'img-rounded', style: content_style, title: "#{bcp.to_s}"), bead_color_path(color)
     end
     content_tag :div, color_string.html_safe, style: "display: inline-block;"
   end
--- app/helpers/bead_colors_helper.rb
+++ app/helpers/bead_colors_helper.rb
@@ -2,7 +2,7 @@ module BeadColorsHelper
     def print_color(color)
     color_string = ''
     content_style = "display: block; float: left;  width: 70px; height: 70px; background-color:rgb(#{color.to_rgb})"
-    color_string += content_tag :span, ' ', style: content_style
+    color_string += content_tag :span, ' ', class: 'img-rounded', style: content_style
     content_tag :div, color_string.html_safe, style: "display: inline-block;"
   end
 end
